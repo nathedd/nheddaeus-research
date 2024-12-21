0a26b82c80ab4819103480f8be193de895731d4d
Nat Heddaeus <nathedd@unc.edu>
2024-12-21 14:17:21 -0500

    Removed unnecessary if-statements from methods.

--- lc_stack.py
+++ lc_stack.py
@@ -50,21 +50,20 @@ def fill_vars():
         filter[int(columns[0])] = (columns[4])  # creates dictionary of {index, filter}
 
 
-def get_zpavg(start, end):  # start, end inclusive, needs to be updated to calculate by filter
+def get_zpavg():  # start, end inclusive, needs to be updated to calculate by filter
     """Helper method for rescale()."""
     tot = 0
     size = 0
     for index in zpdiff:
-        if index in range(start, end + 1):
-            if forceddiffimflux[index] != None and forceddiffimfluxunc[index] != None:  
-                tot += zpdiff[index]
-                size += 1
+        if forceddiffimflux[index] != None and forceddiffimfluxunc[index] != None:  
+            tot += zpdiff[index]
+            size += 1
     return (tot/size)
 
 
-def rescale(start, end):  # start, end inclusive; need to add index out of bounds errors for < first index in list, > last index in list
+def rescale():  # start, end inclusive; need to add index out of bounds errors for < first index in list, > last index in list
     """Given lists of ascii data, make new columns for forcediffimfluxi and forceddiffimfluxunci with rescaled input fluxes and uncertainties."""
-    zpavg = get_zpavg(start, end + 1)
+    zpavg = get_zpavg()
     g_list = []  # list of rescaled fluxes in the g band
     r_list = []  # list of rescaled fluxes in the r band
     i_list = []  # list of rescaled fluxes in the i band
@@ -73,51 +72,36 @@ def rescale(start, end):  # start, end inclusive; need to add index out of bound
     i_unc_list = []  # list of rescaled uncertainties in the i band
 
     for index in forceddiffimflux:  # check calculations
-        if index in range (start, end + 1):
-            if forceddiffimflux[index] != None and forceddiffimfluxunc[index] != None:  # skips unusable data points
-                new = forceddiffimflux[index]*10**(0.4*(zpavg-zpdiff[index]))
-                forceddiffimflux_new[index] = new  # place fluxes on the same photometric zeropoint, may be removable
-                # sorts new flux by filter
-                if filter[index] == 'ZTF_g':
-                    g_list.append(new)
-                elif filter[index] == 'ZTF_r':
-                    r_list.append(new)
-                elif filter[index] == 'ZTF_i':
-                    i_list.append(new)
+        if forceddiffimflux[index] != None and forceddiffimfluxunc[index] != None:  # skips unusable data points
+            new = forceddiffimflux[index]*10**(0.4*(zpavg-zpdiff[index]))
+            forceddiffimflux_new[index] = new  # place fluxes on the same photometric zeropoint, may be removable
+            # sorts new flux by filter
+            if filter[index] == 'ZTF_g':
+                g_list.append(new)
+            elif filter[index] == 'ZTF_r':
+                r_list.append(new)
+            elif filter[index] == 'ZTF_i':
+                i_list.append(new)
     
     flux_by_filter['ZTF_g'] = g_list
     flux_by_filter['ZTF_r'] = r_list
     flux_by_filter['ZTF_i'] = i_list
 
     for index in forceddiffimfluxunc:  # check calculations
-        if index in range(start, end + 1):
-            if forceddiffimfluxunc[index] != None and forceddiffimflux[index] != None:  # skips unusable data points
-                new = forceddiffimfluxunc[index]*10**(0.4*(zpavg-zpdiff[index]))
-                forceddiffimfluxunc_new[index] = new  # place uncertainties on the same photometric zeropoint, may be removable
-                # sorts new uncertainty by filter
-                if filter[index] == 'ZTF_g':
-                    g_unc_list.append(new)
-                elif filter[index] == 'ZTF_r':
-                    r_unc_list.append(new)
-                elif filter[index] == 'ZTF_i':
-                    i_unc_list.append(new)
+        if forceddiffimfluxunc[index] != None and forceddiffimflux[index] != None:  # skips unusable data points
+            new = forceddiffimfluxunc[index]*10**(0.4*(zpavg-zpdiff[index]))
+            forceddiffimfluxunc_new[index] = new  # place uncertainties on the same photometric zeropoint, may be removable
+            # sorts new uncertainty by filter
+            if filter[index] == 'ZTF_g':
+                g_unc_list.append(new)
+            elif filter[index] == 'ZTF_r':
+                r_unc_list.append(new)
+            elif filter[index] == 'ZTF_i':
+                i_unc_list.append(new)
     
     unc_by_filter['ZTF_g'] = g_unc_list
     unc_by_filter['ZTF_r'] = r_unc_list
     unc_by_filter['ZTF_i'] = i_unc_list
-
-
-# def collapse_flux():  # will need to be reworked for specific time window; check calculations
-#     """Assuming that underlying source is stationary within time window, collapse rescaled single-epoch fluxes using an inverse-variance weighted average."""
-#     flux = 0
-#     w_tot = 0
-#     for index in forceddiffimflux_new:
-#         w = 1/(forceddiffimfluxunc_new[index])**2  # weight (uncertainty)
-#         w_tot += w  # summation of weighted uncertainties
-#         flux += w*forceddiffimflux_new[index]
-#     flux = flux / w_tot
-#     flux_unc = w_tot**(-1/2)  # effectively forceddiffimfluxunc_new/sqrt(n)
-#     return flux, flux_unc
 
 
 def collapse_flux_by_filter():  # needs testing
@@ -141,12 +125,7 @@ def collapse_flux_by_filter():  # needs testing
 
 def main():
     fill_vars()
-    #print(get_zpavg(0, 2335))
-    rescale(0, 2335)
-    #print(unc_by_filter)
-    print(forceddiffimfluxunc)
-    #print(forceddiffimfluxunc_new)
-    #print(zpdiff)
+    rescale()
     collapse_flux_by_filter()
 
 main()
