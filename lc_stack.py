33e22cf54ba897e27d7d92c788e3e1e1761d2b08
Nat Heddaeus <nathedd@unc.edu>
2024-12-22 16:20:48 -0500

    Fixed some issues with calculation under collapse_flux_by_filter method.

--- lc_stack.py
+++ lc_stack.py
@@ -16,7 +16,7 @@ import math
 # use of user inputs to select desired binning window
 file_name = str(input("Enter filename: "))
 start = 57 + int(input("Enter starting index (Index will be found in column one of your ascii file, in the rows that do not start with '#'): "))  # starting line 57 appears to be consistent across ZTF files, but may need to be changed in future updates
-end = 58 + int(input("Enter ending index: "))
+end = 58 + int(input("Enter ending index: "))  # readlines method appears to be end exclusive, hence 57 + 1
 
 with open(file_name) as f: # opens .txt file
     data = f.readlines()[start: end]
@@ -25,29 +25,33 @@ with open(file_name) as f: # opens .txt file
 jd = {}  # julian day; not used in these methods but may be passed to other files
 forceddiffimflux = {}
 forceddiffimfluxunc = {}
-zpdiff = {}
+zpdiff = {}  # zeropoint
 filter = {}
 
-forceddiffimflux_new = {}
+forceddiffimflux_new = {}  # not used in these methods but may be passed to other files
 flux_by_filter = {}  # {filter, list of flux}
-forceddiffimfluxunc_new = {}
+forceddiffimfluxunc_new = {}  # not used in these methods but may be passed to other files
 unc_by_filter = {}  # {filter, list of unc}
 
 
-def fill_vars():
+def fill_vars():  # null value replacement with None may be unneccessary now
     "Takes a string of ascii data and converts it to dictionary variables."
     for line in data:
         line = line.strip()
         columns = line.split()
+
         jd[int(columns[0])] = (float(columns[22]))  # creates dictionary of {index, julian day}
+        
         if (columns[24] != 'null'): 
             forceddiffimflux[int(columns[0])] = (float(columns[24]))  # creates dictionary of {index, flux}
         else:
             forceddiffimflux[int(columns[0])] = (None)  # catches unusable data points
+        
         if (columns[25] != 'null'): 
             forceddiffimfluxunc[int(columns[0])] = (float(columns[25]))  # creates dictionary of {index, flux uncertainty}
         else:
             forceddiffimflux[int(columns[0])] = (None)  # catches unusable data points
+        
         zpdiff[int(columns[0])] = (float(columns[20]))  # creates dictionary of {index, zero points}
         filter[int(columns[0])] = (columns[4])  # creates dictionary of {index, filter}
 
@@ -65,7 +69,7 @@ def get_zpavg():  # start, end inclusive, needs to be updated to calculate by fi
 
 def rescale():  # start, end inclusive; need to add index out of bounds errors for < first index in list, > last index in list
     """Given lists of ascii data, make new columns for forcediffimfluxi and forceddiffimfluxunci with rescaled input fluxes and uncertainties."""
-    zpavg = get_zpavg()
+    zpavg =  28.9891
     g_list = []  # list of rescaled fluxes in the g band
     r_list = []  # list of rescaled fluxes in the r band
     i_list = []  # list of rescaled fluxes in the i band
@@ -77,11 +81,14 @@ def rescale():  # start, end inclusive; need to add index out of bounds errors f
         if forceddiffimflux[index] != None and forceddiffimfluxunc[index] != None:  # skips unusable data points
             new = forceddiffimflux[index]*10**(0.4*(zpavg-zpdiff[index]))
             forceddiffimflux_new[index] = new  # place fluxes on the same photometric zeropoint, may be removable
+            
             # sorts new flux by filter
             if filter[index] == 'ZTF_g':
                 g_list.append(new)
+            
             elif filter[index] == 'ZTF_r':
                 r_list.append(new)
+            
             elif filter[index] == 'ZTF_i':
                 i_list.append(new)
     
@@ -96,8 +103,10 @@ def rescale():  # start, end inclusive; need to add index out of bounds errors f
             # sorts new uncertainty by filter
             if filter[index] == 'ZTF_g':
                 g_unc_list.append(new)
+            
             elif filter[index] == 'ZTF_r':
                 r_unc_list.append(new)
+            
             elif filter[index] == 'ZTF_i':
                 i_unc_list.append(new)
     
@@ -111,10 +120,14 @@ def collapse_flux_by_filter():  # needs testing
     for filter in flux_by_filter:
         flux = 0
         w_tot = 0
+        idx = 0
+        
         for point in flux_by_filter[filter]:
-            w = 1/(point)**2  # weight (uncertainty)
-            w_tot += w
+            w = 1/((unc_by_filter[filter][idx])**2)
             flux += w*point
+            w_tot += w
+            idx += 1
+
         if w_tot != 0:
             flux = flux / w_tot
             flux_unc = w_tot**(-1/2) 
@@ -136,6 +149,5 @@ def main():
     fill_vars()
     rescale()
     collapse_flux_by_filter()
-    print(zpdiff)
 
 main()
