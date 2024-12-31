a6b586997f4ff70cb2bda41d454aed0f6fccb622
Nat Heddaeus <nathedd@unc.edu>
2024-12-31 13:26:25 -0500

    Removed extra blank lines.

--- lc_stack.py
+++ lc_stack.py
@@ -4,7 +4,7 @@
 """
 Filename: lc_stack.py
 Author: Nat Heddaeus
-Date: 2024-12-30
+Date: 2024-12-31
 Version: 1.0
 Description: Given a dataframe with a maxlike light curve, stack the flux. Based on guidelines from "Generating Lightcurves from Forced PSF-fit Photometry on ZTF Difference Images" by Masci et. al, 2022.
 
@@ -13,7 +13,6 @@ Contact: nathedd@unc.edu
 
 import math
 import matplotlib.pyplot as plt
-
 
 # storing variable data as dictionaries: key = index, value = column of file_name
 jd = {}  # julian day; not used in these methods but may be passed to other files
@@ -36,24 +35,19 @@ def fill_vars(data):
     for line in data:
         line = line.strip()
         columns = line.split()
-
         jd[int(columns[0])] = (float(columns[22]))  # creates dictionary of {index, julian day}
-        
         if (columns[24] != 'null'): 
             forcediffimflux[int(columns[0])] = (float(columns[24]))  # creates dictionary of {index, flux}
         else:
-            forcediffimflux[int(columns[0])] = (None)  # catches unusable data points; fills with None for consistency between variables
-        
+            forcediffimflux[int(columns[0])] = (None)  # catches unusable data points; fills with None for consistency between variables 
         if (columns[25] != 'null'): 
             forcediffimfluxunc[int(columns[0])] = (float(columns[25]))  # creates dictionary of {index, flux uncertainty}
         else:
             forcediffimflux[int(columns[0])] = (None)  # catches unusable data points; fills with None for consistency between variables
-       
         if (columns[27] != 'null'):
             forceddiffimchisq[int(columns[0])] = (float(columns[27]))  # creates dictionary of {index, chi^2}
         else:
             forceddiffimchisq[int(columns[0])] = (None)  # catches unusable data points; fills with None for consistency between variables
-        
         zpdiff[int(columns[0])] = (float(columns[20]))  # creates dictionary of {index, zero points}
         filter[int(columns[0])] = (columns[4])  # creates dictionary of {index, filter}
 
@@ -70,31 +64,25 @@ def validate_uncertainties():
     g_list = []
     r_list = []
     i_list = []
-
     # placeholders
     g_avg = 1
     r_avg = 1
     i_avg = 1
-    
     for index in forceddiffimchisq: # sort chi squared by filter
         if forceddiffimchisq[index] != None:
             new = forceddiffimchisq[index]
             if filter[index] == 'ZTF_g':
                 g_list.append(new)
-            
             elif filter[index] == 'ZTF_r':
                 r_list.append(new)
-            
             elif filter[index] == 'ZTF_i':
                 i_list.append(new)
-    
     if len(g_list) != 0:
         g_avg = sum(g_list) / len(g_list)
     if len(r_list) != 0:
         r_avg = sum(r_list) / len(r_list)
     if len(i_list) != 0:
         i_avg = sum(i_list) / len(i_list)
-
 # ensure pixel uncertainties are consistent
     if round(g_avg) != 1:
         for index in forcediffimfluxunc:
@@ -118,26 +106,20 @@ def rescale():  # start, end inclusive
     g_unc_list = []  # list of rescaled uncertainties in the g band
     r_unc_list = []  # list of rescaled uncertainties in the r band
     i_unc_list = []  # list of rescaled uncertainties in the i band
-
     for index in forcediffimflux:  
         if forcediffimflux[index] != None and forcediffimfluxunc[index] != None:  # skips unusable data points
             new = forcediffimflux[index]*10**(0.4*(zpavg-zpdiff[index]))  # place fluxes on the same photometric zeropoint
             forcediffimflux_new[index] = new
-            
             # sorts new flux by filter and matches dictionary of jd to flux by filter by index
             if filter[index] == 'ZTF_g':
                 g_list.append(new)
-            
             elif filter[index] == 'ZTF_r':
                 r_list.append(new)
-            
             elif filter[index] == 'ZTF_i':
                 i_list.append(new)
-    
     flux_by_filter['ZTF_g'] = g_list
     flux_by_filter['ZTF_r'] = r_list
     flux_by_filter['ZTF_i'] = i_list
-
     for index in forcediffimfluxunc:
         if forcediffimfluxunc[index] != None and forcediffimflux[index] != None:  # skips unusable data points
             new = forcediffimfluxunc[index]*10**(0.4*(zpavg-zpdiff[index]))  # place uncertainties on the same photometric zeropoint
@@ -145,13 +127,10 @@ def rescale():  # start, end inclusive
             # sorts new uncertainty by filter
             if filter[index] == 'ZTF_g':
                 g_unc_list.append(new)
-            
             elif filter[index] == 'ZTF_r':
                 r_unc_list.append(new)
-            
             elif filter[index] == 'ZTF_i':
                 i_unc_list.append(new)
-    
     unc_by_filter['ZTF_g'] = g_unc_list
     unc_by_filter['ZTF_r'] = r_unc_list
     unc_by_filter['ZTF_i'] = i_unc_list
@@ -163,18 +142,15 @@ def collapse_flux_by_filter(start, end):
         flux = 0
         w_tot = 0
         idx = 0
-        
         for point in flux_by_filter[filter]:
             w = 1/((unc_by_filter[filter][idx])**2)
             flux += w*point
             w_tot += w
             idx += 1
-
         if w_tot != 0:
             flux = flux / w_tot
             flux_unc = w_tot**(-1/2) 
             print(filter + ' flux: ' + str(flux) + ' flux_unc: ' + str(flux_unc)) 
-        
         combined_meas[filter] = [flux, flux_unc, jd[start - 57], jd[end - 58]]  # if not using a ZTF file, have these match the indices used in jd dictionary
 
 
@@ -211,7 +187,6 @@ def main():
     start = 57 + int(input("Enter starting index (Index will be found in column one of your ascii file, in the rows that do not start with '#'): "))  # starting line 57 appears to be consistent across ZTF files, but may need to be changed in future updates; note: if using a file that is not of ZTF format, indices on lines 20 and 21 of this code will need to be edited to match your file
     end = 58 + int(input("Enter ending index: "))  # readlines method appears to be end exclusive, hence 57 + 1; 
     baseline = float(input("If there is any residual baseline (nonzero), input it here. Else, input 0: "))  # you will need to have examined a plot of forcediffimflux to jd to determine if there is any residual offset in the baseline (Masci et. al section 10)
-
     with open(file_name) as f: # opens .txt file
         data = f.readlines()[start: end]
     fill_vars(data)
