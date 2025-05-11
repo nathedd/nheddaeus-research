a1e87fa14af638541f0b337dbaa3a5dee1c02a16
Nat Heddaeus <nathedd@unc.edu>
2025-05-11 15:56:56 -0400

    Fixed issue where indices of combined flux were not storing correctly.

--- lc_stack_int.py
+++ lc_stack_int.py
@@ -4,7 +4,7 @@
 """
 Filename: lc_stack_int.py
 Author: Nat Heddaeus
-Date: 2024-04-30
+Date: 2024-05-11
 Version: 1.0
 Description: An optimized version of the code from lc_stack.py made for integration into the ztfrest pipeline. Given a dataframe with a maxlike light curve, stack the flux. Based on guidelines from "Generating Lightcurves from Forced PSF-fit Photometry on ZTF Difference Images" by Masci et. al, 2022. 
 
@@ -38,7 +38,6 @@ def stack_lc(tbl, days_stack):
     bins = np.arange(jd[0] + days_stack, jd[length] + days_stack, days_stack)  # creates a bin for every day between the start and end date with mesh size of days_stack
     bin_len = len(bins)
 
-    # correct baseline, validate uncertainties, rescale, collapse flux by filter, cal mag
 
     # place the fluxes on the same photometric zeropoint
     rs_flux = np.zeros(length+1)  # variable for rescaled fluxes
@@ -61,13 +60,17 @@ def stack_lc(tbl, days_stack):
     bin_unc = np.zeros((bin_len, 2))
     for i in range(0, len(filters)):
         f_idx = np.where(fil==filters[i])[0]  # get all indices for a particular filter
-        jd_bin = jd[f_idx]
         for j in range(0, bin_len):
-            bin_idx = np.where(jd_bin <= bins[j])[0]
-            bin_idx = np.where(jd_bin[bin_idx] > bins[j-1])[0]
-            w = 1/(rs_unc[bin_idx])**2
+            new_idx = np.copy(f_idx)
+            bin_idx = np.where(jd[new_idx] <= bins[j])[0]  # get valid lower limit indices in jd_bin
+            if bin_idx.size != 0:
+                new_idx = new_idx[bin_idx]
+            if j != 0 and new_idx.size != 0:
+                bin_idx = np.where(jd[new_idx] > bins[j-1])[0]  # get valid upper limit indices in jd_bin
+                new_idx = new_idx[bin_idx]
+            w = 1/(rs_unc[new_idx])**2
             if w.size > 0:
-                bin_flux[j, 0] = np.nansum(w*rs_flux[bin_idx])/np.nansum(w)
+                bin_flux[j, 0] = np.nansum(w*rs_flux[new_idx])/np.nansum(w)
                 bin_unc[j, 0] = np.nansum(w)**(-1/2)  # combined unc
                 bin_flux[j, 1] = i  # filter
                 bin_unc[j, 1] = i  # filter
@@ -81,24 +84,39 @@ def stack_lc(tbl, days_stack):
         unc_i = bin_unc[i, 0]
         if flux_i != 0:
             if (flux_i/unc_i) > snt_det:  # confident detection
-                mag[i] = zpavg - 2.5*math.log[bin_flux[i]]
+                mag[i] = zpavg - 2.5*math.log(flux_i)
                 sigma[i] = 1.0857*unc_i/flux_i
                 flux_ul[i] = np.nan
             else:
-                flux_ul[i] = zpavg - 2.5*math.log[snt_ul*unc_i]  # compute flux upper limit
+                flux_ul[i] = zpavg - 2.5*math.log(snt_ul*unc_i)  # compute flux upper limit
                 mag[i] = np.nan
                 sigma[i] = np.nan
         else:
+            flux_ul[i] = np.nan
             mag[i] = np.nan
             sigma[i] = np.nan
-    
 
-    # plot calibrated magnitudes
+    # plot
+    r = np.where(bin_flux[:, 1] == 0)
+    plt.scatter(bins[r], mag[r], c='blue')
+    plt.errorbar(bins[r], mag[r], yerr=sigma[r], ls='none', color='blue')
 
-    print('yello')
+    plt.scatter(bins[r], flux_ul[r], marker='v', color='blue')
 
+    g = np.where(bin_flux[:, 1] == 1)
+    plt.scatter(bins[g], mag[g], c='red')
+    plt.errorbar(bins[g], mag[g], yerr=sigma[g], ls='none', color='red')
 
+    plt.scatter(bins[g], flux_ul[g], marker='v', color='red')
 
+    b = np.where(bin_flux[:, 1] == 2)
+    plt.scatter(bins[b], mag[b], c='green')
+    plt.errorbar(bins[b], mag[b], yerr=sigma[b], ls='none', color='green')
+
+    plt.scatter(bins[b], flux_ul[b], marker='v', color='green')
+
+    plt.gca().invert_yaxis()
+    plt.show()
 
 
 
